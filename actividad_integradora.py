e0151093255ee9ac64522944a4a68db3236446b0
BetoRdz01 <110861114+BetoRdz01@users.noreply.github.com>
2023-06-08 13:10:01 -0600

    Update actividad_integradora.py

--- actividad_integradora.py
+++ actividad_integradora.py
@@ -13,8 +13,13 @@ import pandas as pd
 import streamlit as st
 
 df = pd.read_csv('https://drive.google.com/file/d/1WQ5QFkP9S1FfpAxCTxQpzWKEP5lD67uL/view?usp=drive_link')
+st.title('The Police Incident Reports from 2028 to 2022 in San Francisco.')
+st.markdown('The data shon below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case sucha as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution')
 
 mapa = pd.DataFrame()
+mapa = mapa.dropna()
+st.map(mapa.astype(int))
+
 '''
 mapa['Date']= df['Incident Date']
 mapa['Day'] = df['Incident Day of Week']
@@ -26,8 +31,3 @@ mapa['Resolution']=df['Resolution']
 mapa['lat']=df['Latitude']
 mapa['lon']=df['Longitude']
 '''
-mapa = mapa.dropna()
-st.map(mapa.astype(int))
-
-st.title('The Police Incident Reports from 2028 to 2022 in San Francisco.')
-st.markdown('The data shon below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case sucha as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution')
